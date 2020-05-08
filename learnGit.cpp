c1b07a35b6733ee0823f9ac87b007227c198a1f7
musematics <708851924@qq.com>
2020-05-08 20:34:13 +0800

    add vector

--- learnGit.cpp
+++ learnGit.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 
 void main(){
     //cout<<" Hello World !" <<endl;
     int happyValue = 0;
+    vector<int> happyArr(10,0);
     cout << happyValue * happyValue <<endl;
     cout<<" Happy Day! " <<endl;
     system("pause");
