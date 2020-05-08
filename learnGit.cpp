38b496ea4bbad666824b00fe5be05870412146cc
musematics <708851924@qq.com>
2020-05-08 17:15:25 +0800

    Add Calculate

--- learnGit.cpp
+++ learnGit.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void main(){
     //cout<<" Hello World !" <<endl;
     int happyValue = 0;
+    cout << happyValue * happyValue <<endl;
     cout<<" Happy Day! " <<endl;
     system("pause");
 }
